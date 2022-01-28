3d20f5209e029d7cf9b21d4cb07b90a21781d1c5
Nima Pourjafar <nima.pourjafar123@gmail.com>
2022-01-28 18:36:33 -0500

    login flow owrks

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import typer
+from gcsa.google_calendar import GoogleCalendar
 
 app = typer.Typer()
 
+global gc
+
 @app.command()
-def hello():
-    print("hello World!")
+def login():
+    gc = GoogleCalendar(credentials_path='./client_secret_176083467420-23cn6d5glp7cr1b4l3sj9t4e59s64hpu.apps.googleusercontent.com.json')
 
 if __name__ == "__main__":
     app()
