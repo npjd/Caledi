05c15a07a0ee5b053e3bc38e5afcff812bd77454
Nima Pourjafar <nima.pourjafar123@gmail.com>
2022-01-29 13:55:26 -0500

    planning events work

--- main.py
+++ main.py
@@ -1,13 +1,49 @@
 import typer
+from datetime import date
+from beautiful_date import *
 from gcsa.google_calendar import GoogleCalendar
+from gcsa.event import Event
+from gcsa.recurrence import Recurrence
+
+from gcsa.recurrence import SECONDLY, MINUTELY, HOURLY, DAILY, WEEKLY, MONTHLY, YEARLY
+
 
 app = typer.Typer()
 
-global gc
 
 @app.command()
-def login():
-    gc = GoogleCalendar(credentials_path='./client_secret_176083467420-23cn6d5glp7cr1b4l3sj9t4e59s64hpu.apps.googleusercontent.com.json')
+def event(time:str, name:str, duration:int = 60):
+    today = date.today()
+
+    startHour,startMinute = time.split(':')
+
+    start = (today.day/M[today.month]/today.year)[int(startHour):int(startMinute)]
+
+    end = start + duration * minutes
+
+    event = Event(name,start=start,end=end)
+
+    gc.add_event(event)
+
+@app.command()
+def event(time:str, name:str,freq:str = 'd', duration:int = 60):
+    today = date.today()
+
+    startHour,startMinute = time.split(':')
+
+    start = (today.day/M[today.month]/today.year)[int(startHour):int(startMinute)]
+
+    end = start + duration * minutes
+
+    if freq.lower() == 'd':
+        event = Event(name,start=start,end=end, recurrence=Recurrence.rule(freq=DAILY))
+    elif freq.lower() == 'w':
+        event = Event(name,start=start,end=end, recurrence=Recurrence.rule(freq=WEEKLY))
+    elif freq.lower() == 'm':
+        event = Event(name,start=start,end=end, recurrence=Recurrence.rule(freq=MONTHLY))
+    
+    gc.add_event(event)
 
 if __name__ == "__main__":
+    gc = GoogleCalendar(credentials_path='./client_secret_176083467420-23cn6d5glp7cr1b4l3sj9t4e59s64hpu.apps.googleusercontent.com.json')
     app()
